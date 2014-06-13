1bcd6fb8af9a10422c9117b4aab92d7125b4ec83
Lawrence Manfredi <lrmanfre@me.com>
2014-06-13 18:01:14 -0400

    5 pendings tests on User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe User do
+  it "is valid with all these first, last name, email, and password"
+  it "is invalid without a first_name"
+  it "is invalid without a last_name"
+  it "is invalid without an email"
+  it "is invalid without a password"
+end
