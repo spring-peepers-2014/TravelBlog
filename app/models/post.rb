5c3908a07ea6996712310e97be286e30a1f711b8
Lawrence Manfredi <lrmanfre@me.com>
2014-06-13 11:23:23 -0400

    reimplement neal's changes
    -got rid of links from trip to posts and photos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :location_pin
-  belongs_to :trip
 
   validates :location_pin, :presence => true
-  validates :trip, :presence => true
-  
+
   validates :title, length: { maximum: 75,
     too_long: "%{count} characters is the maximum allowed" }
   validates :body, length: { maximum: 1000,
