ff62b21f13810de68b62067069013911921052ac
Lawrence Manfredi <lrmanfre@me.com>
2014-06-13 14:59:25 -0400

    add bcrypt methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,23 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, :uniqueness => true, :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :password_digest, presence: true, :length => { :minimum => 6, :message => "must be at least 6 characters" }
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(pass)
+    @password = Password.create(pass)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(email, pass)
+    @user = User.find_by_email(email)
+    if @user && (@user.password == pass)
+      @user
+    else
+      nil
+    end
+  end
+
 end
