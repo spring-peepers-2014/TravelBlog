d8e23fc397336406e973b90adbb689050a089e96
nealfennimore <fennimoren@gmail.com>
2014-06-13 14:20:26 -0400

    Missing s on trips resource routes generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "trips#index"
-  resources :trip, :only => [:new, :create, :index]
+  resources :trips, :only => [:new, :create, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
