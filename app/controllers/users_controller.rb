07c9f72361a280cdc65d49726329d0ee0667b222
Lawrence Manfredi <lrmanfre@me.com>
2014-06-13 15:25:00 -0400

    add #user_params to handle Rails 4 need for strong params
    -also modded line that generates a new User to call #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
@@ -31,5 +31,11 @@ class UsersController < ApplicationController
   def logout
     session.clear
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
