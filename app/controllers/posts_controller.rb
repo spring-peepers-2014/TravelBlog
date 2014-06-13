66e1db6a86d242788b9bc0e9e3e65bcc949d21a2
Christine Feaster <feasterc@gmail.com>
2014-06-13 18:21:37 -0400

    Add view post and view all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,17 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Trip.find(params[:trip_id]).posts
   end
 
   def new
   end
 
   def create
+
   end
 
   def show
+    @post = Post.find(params[:post_id])
   end
 
   def edit
