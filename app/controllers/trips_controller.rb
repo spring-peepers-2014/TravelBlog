0799232212126149061cc7f67a83cd86cf7262df
Lawrence Manfredi <lrmanfre@me.com>
2014-06-13 12:16:11 -0400

    now able to create a trip and location_pin in DB

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,6 +2,7 @@
 class TripsController < ApplicationController
 
   def index
+    @trips = Trip.all
   end
 
   def new
@@ -9,14 +10,9 @@ class TripsController < ApplicationController
   end
 
   def create
-
-    # if (Map.all).nil?
-    #   @map = Map.create(name: "USA")
-    # end
-
-    @trip = Trip.create(title: params[:trip][:title])
+    @trip = Trip.create(title: params[:trip][:title], user_id: 1)
     @coords =  Geocoder.coordinates(params[:trip][:title])
-    @location_pin = LocationPin.create(location_name: params[:trip][:title], latitude: @coords[0], longitude: @coords[1])
-
+    @location_pin = LocationPin.create(location_name: params[:trip][:title], latitude: @coords[0], longitude: @coords[1], trip_id: 1, map_id: 1)
+    redirect_to root_path
   end
 end
