ac569d250073af9d354bcc39b6d1b89db192a806
Christine Feaster <feasterc@gmail.com>
2014-06-13 17:53:19 -0400

    Add notes on post and photo shallow routes and path names

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,66 @@
 Rails.application.routes.draw do
-  root to: "trips#index"
+  root "trips#index"
   resources :users, :only => [:new, :create]
   resources :trips, :only => [:new, :create, :index]
+  resources :posts, :only => [:show, :edit, :update, :destroy]
+  resources :photos, :only => [:show, :edit, :update, :destroy]
+
   resources :trips, shallow: true do
-    resources :posts
+    resources :posts, :only => [:new, :create, :index]
+    resources :photos, :only => [:new, :create, :index]
   end
+
 
   post '/users/login', to: 'users#login'
   get '/users/logout', to: 'users#logout'
 
 end
+
+# path_name
+# controller actions
+
+# POSTS
+
+# trip_posts
+# get '/trips/:trip_id/posts(.:format)' => posts#index
+
+# no_path_name
+# post '/trips/:trip_id/posts(.:format)' => posts#create
+
+# new_trip_post
+# get '/trips/:trip_id/posts/new(.:format)' => posts#new
+
+# edit_post
+# get '/posts/:id/edit(.:format)' => posts#edit
+
+# post
+# get '/posts/:id(.:format)' => posts#show
+
+# no_path_name
+# put '/posts/:id(.:format)'  => posts#update
+
+# no_path_name
+# delete '/posts/:id(.:format)' => posts#destroy
+
+# PHOTOS
+
+# trip_photos
+# get '/trips/:trip_id/photos(.:format)' => photos#index
+
+# no_path_name
+# post '/trips/:trip_id/photos(.:format)' => photos#create
+
+# new_trip_photo
+# get '/trips/:trip_id/photos/new(.:format)' => photos#new
+
+# edit_post
+# get '/photos/:id/edit(.:format)' => photos#edit
+
+# photo
+# get '/photos/:id(.:format)' => photos#show
+
+# no_path_name
+# put '/photos/:id(.:format)'  => photos#update
+
+# no_path_name
+# delete '/photos/:id(.:format)' => photos#destroy
