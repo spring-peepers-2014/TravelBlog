e8fdd512a3fc19d2237faa31032ce2385f915865
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-14 15:06:46 -0400

    Created trip model tests

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Trip do
-  it "is valid with a title and a user"
-  it "is invalid without a title"
-  it "is invalid without an associated user"
+  it "is valid with a title and a user" do
+    expect(build(:trip)).to be_valid
+  end
+
+  it "is invalid without a title" do
+    expect(build(:trip, title: nil)).to_not be_valid
+  end
+
+  it "is invalid without an associated user" do
+    expect(build(:trip, user_id: nil)).to_not be_valid
+  end
 end
