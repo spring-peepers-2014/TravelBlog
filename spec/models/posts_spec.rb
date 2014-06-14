b24970c5ac8532500ee0248e0b0dafbe1b839ff6
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-14 14:41:27 -0400

    Passing post validation tests

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -4,13 +4,16 @@ describe Post do
   it "has a valid factory" do
     expect(build(:post)).to be_valid
   end
+
   it "is invalid without a location" do
-    expect(build(:post, location_pin_id: nil)).to be_invalid 
+    expect(build(:post, location_pin_id: nil)).to_not be_valid
   end
+
   it "is invalid without a title" do
-    expect(build(:post, title: nil)).to be_invalid
+    expect(build(:post, title: nil)).to_not be_valid
   end
+
   it "is invalid without a body" do
-    expect(build(:post, body: nil)).to be_invalid
+    expect(build(:post, body: nil)).to_not be_valid
   end
 end
