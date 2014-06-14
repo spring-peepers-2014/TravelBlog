b7a4675be3839c2f90ec0162285a8195c046704b
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-14 16:40:46 -0400

    Added presence true validation for title and body - This was done to make the tests for post model tests to pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
 
   validates :location_pin, :presence => true
   
-  validates :title, length: { maximum: 75,
+  validates :title, presence: true, length: { maximum: 75,
     too_long: "%{count} characters is the maximum allowed" }
-  validates :body, length: { maximum: 1000,
+  validates :body, presence: true, length: { maximum: 1000,
     too_long: "%{count} characters is the maximum allowed" }
 end
