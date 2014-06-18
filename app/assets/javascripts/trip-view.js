ae888fa6884385b3df9536472e248f0a8094169e
nealfennimore <fennimoren@gmail.com>
2014-06-18 11:54:19 -0400

    Trip JS changes:
    - Modified controller- Added view and model
    - Removed soup from controller
    - Added addLocation and addTrip methods
    - Added Trip.Presenter to Trip view
    - Can display formatted html is sidebar by calling presentLocation/Trip
    - js_location.js has listeners to create location and trip // needs re-work later

--- app/assets/javascripts/trip-view.js
+++ app/assets/javascripts/trip-view.js
@@ -1,16 +1,42 @@
-Trip = {
-  addLocation: function(tripid, data){
-    var url ='/trips/'+ tripid +'/locations';
-    var type = "post";
-    (Map.getAjaxData(url, type, data)).done( function(resp) {
-      Map.addPin(Map.getPosition(resp.trip.lat, resp.trip.lon));
-      Trip.appendToTripList(resp.trip.id, resp.trip.name);
-    });
-  },
-  appendToTripList: function(tripid, name){
-  $("#trip-show-container").append("<div class='list-group'><div class='list-group-item'><h3 class='location-names list-group-item-heading'><a href='/locations/"+tripid+"'>"+name+"</a></h3></div></div>");
-  },
+Trip.Presenter = function(trip_name, trip_id, root, second, third){
+  this.trip_name = trip_name;
+  this.trip_id = trip_id;
+  this.rootNodeType = root || "li";
+  this.secondNodeType = second || "a";
+  this.thirdNodeType = third || "div";
 };
 
+Trip.Presenter.prototype = {
+  presentTrip: function() {
+    var rootNode = document.createElement(this.rootNodeType),
+        secondNode = document.createElement(this.secondNodeType);
 
+      rootNode.className = "list-group-item";
 
+      secondNode.setAttribute('href', '/trips/' + this.trip_id);
+      secondNode.textContent = this.trip_name;
+
+      rootNode.appendChild(secondNode);
+
+      return rootNode;
+  },
+  presentLocation: function() {
+  var h3_tag = document.createElement(this.rootNodeType),
+      a_tag = document.createElement(this.secondNodeType);
+      list_group = document.createElement(this.thirdNodeType);
+      list_group_item = document.createElement(this.thirdNodeType);
+
+    list_group.className = "list-group";
+    list_group_item.className = "list-group-item";
+    h3_tag.className = "location-names list-group-item-heading";
+
+    a_tag.setAttribute('href', '/locations/' + this.trip_id);
+    a_tag.textContent = this.trip_name;
+
+    list_group.appendChild(list_group_item);
+    list_group_item.appendChild(h3_tag);
+    h3_tag.appendChild(a_tag);
+
+    return list_group;
+  }
+};
