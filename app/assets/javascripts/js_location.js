7bf855097bc7947d5f21247c6e720aef2f2be003
Git Moar Coffee <Fennimoren@gmail.com>
2014-06-17 21:36:18 -0400

    Testing to get addLocation to work

--- app/assets/javascripts/js_location.js
+++ app/assets/javascripts/js_location.js
@@ -1,30 +1,12 @@
 $(document).ready(function() {
 
-    $("#add-location").on("click", function(e) {
+    $("#location-create").on("submit", function(e) {
         e.preventDefault();
-        $("#location-create").toggleClass("hidden");
+        var trip_id = $('#map').data('trip-id');
+        var data = {
+            location: $("#location-create input[type='text']").val()
+        };
+        Trip.addLocation( trip_id, data )
     });
-
-        $("#location-create").on("submit", function(e) {
-            e.preventDefault();
-            var trip_id = $('#auto_load_pins').data('trip-id');
-            var data = {
-                location: $("input[type='text']").val()
-            };
-
-            $.ajax({
-                url: '/trips/'+ trip_id +'/locations',
-                type: "post",
-                data: data,
-                dataType: "json"
-            }).done(function(resp) {
-                $("#trip-show-container").append("<div class='list-group'><div class='list-group-item'><h3 class='location-names list-group-item-heading'><a href='/locations/"+resp.trip.id+"'>"+resp.trip.name+"</a></h3></div></div>");
-
-                $("#location-create").hide();
-                 Map.addPin(Map.getPosition(resp.trip.lat, resp.trip.lon))
-            });
-    });
-
 
 });
-
