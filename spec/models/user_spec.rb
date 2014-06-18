54dd5f7ba24e4401546337cc9c0095464e7e69dc
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-18 11:29:25 -0400

    Fixed faulty user tests - Gave a password limit to Faker password data in User factory - Refactored User spec to use Factory Girl - Added test to User spec to test for password min length of 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,51 +2,39 @@ require 'spec_helper'
 
 describe User do
   it "is valid with all these first, last name, email, and password" do
-    user = User.new(first_name: "Kenneth",
-                    last_name: "Uy",
-                    email: "missingno15@gmail.com",
-                    password: "123456")
-    expect(user.save).to equal(true)
+    expect(build(:user)).to be_valid
   end
 
   it "is invalid without a first_name" do
-    user = User.new(last_name: "Uy",
-                    email: "missingno15@gmail.com",
-                    password: "123456")
-    expect{ user.save }.to raise_error
+    expect(build(:user, first_name: nil)).to_not be_valid
   end
 
   it "is invalid without a last_name" do
-    user = User.new(first_name: "Kenneth",
-                    email: "missingno15@gmail.com",
-                    password: "123456")
-    expect{ user.save }.to raise_error
+    expect(build(:user, last_name: nil)).to_not be_valid
   end
 
   it "is invalid without an email" do
-    user = User.new(first_name: "Kenneth",
-                    last_name: "Uy",
-                    password: "123456")
-    expect(user.save).to equal(false)
+    expect(build(:user, email: nil)).to_not be_valid
   end
 
   it "is invalid without a password" do
-    user = User.new(first_name: "Kenneth",
-                    last_name: "Uy",
-                    email: "missingno15@gmail.com")
-    expect{user.save}.to raise_error
+    expect(build(:user, password: nil)).to_not be_valid
+  end
+
+  it "is invalid with a password less than 6 characters" do
+    expect(build(:user, password: "god")).to_not be_valid
   end
 
   it "does not accept duplicate emails" do
-    original = User.create(first_name: "Kenneth",
-                    last_name: "Uy",
-                    email: "missingno15@gmail.com",
-                    password: "123456")
+    original = User.create(first_name: "Shmitty",
+                    last_name: "Werberjaegarmanjensen",
+                    email: "hewasnumberone@gmail.com",
+                    password: "lovesexsecretgod")
 
-    duplicate = User.new(first_name: "Kenneth",
-                    last_name: "Uy",
-                    email: "missingno15@gmail.com",
-                    password: "123456")
-    expect(duplicate.save).to equal(false)
+    duplicate = User.new(first_name: "Shmitty",
+                    last_name: "Werberjaegarmanjsen",
+                    email: "hewasnumberone@gmail.com",
+                    password: "lovesexsecretgod")
+    expect(duplicate).to_not be_valid
   end
 end
