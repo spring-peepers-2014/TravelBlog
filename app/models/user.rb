1de089f6254d7e20340fb9c25b88edfb4ba1e736
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-18 11:59:29 -0400

    Added validations for :password - Added presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,8 @@ class User < ActiveRecord::Base
   has_many :posts, through: :trips
   has_many :photos, through: :trips
   
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => true, :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :password, presence: true, :length => { :minimum => 6, :message => "must be at least 6 characters" }
   validates :password_digest, presence: true, :length => { :minimum => 6, :message => "must be at least 6 characters" }
 
   def password
