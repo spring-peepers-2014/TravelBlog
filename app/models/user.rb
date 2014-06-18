7f17dd73128225eb2920a3dd1a90672fa4eeafbe
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-18 11:32:39 -0400

    Added presence validation for first_name and last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
   has_many :location_pins, through: :trips
   has_many :posts, through: :trips
   has_many :photos, through: :trips
-
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => true, :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :password_digest, presence: true, :length => { :minimum => 6, :message => "must be at least 6 characters" }
 
