caad12f6614726fc7caf734c350af7227a2187dd
Christine Feaster <feasterc@gmail.com>
2014-06-18 12:28:26 -0400

    Remove placeholder app-initializer.js, not functional

--- app/assets/javascripts/initialize-app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    mapController = new Map.Controller();
-    pinController = new LocationPin.Controller();
-    tripController = new Trip.Controller();
-    postController = new Post.Controller();
-});
