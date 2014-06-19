3b03bc104bb54ecd44fad28f2011f79681003737
nealfennimore <fennimoren@gmail.com>
2014-06-18 22:34:27 -0400

    Reverted map controller back - tests were accidently pushed

--- app/assets/javascripts/map-controller.js
+++ app/assets/javascripts/map-controller.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
   var item_selector = document.getElementById("map-canvas");
   Map.init(item_selector, mapOptions);
-
-  var marker = new google.maps.Marker({
-      map: Map.gMap
-    });
-
-
-  google.maps.event.addListener(marker, "click", function() {
-    console.log("hello");
-  });
 });
