29d33540ee97d15d5c34b4a5db11ba3405027023
Christine Feaster <feasterc@gmail.com>
2014-06-18 21:28:41 -0400

    Refactor JS Map model and dependencies
    Remove redundant createPin function form Map.
    Map.loadTripPins queries location_pins#index for pins instead of trips#show_markers.
    Trip view and Trip controller update to handle both http and ajax requests.
    Updated routes, removed trips#show_marker.
    Refactored JS LocationPin model.

--- app/assets/javascripts/map-model.js
+++ app/assets/javascripts/map-model.js
@@ -1,41 +1,33 @@
 Map = {
-  init: function(selector, options) {
-    this.allPins = [];
-    this.gMap = google.maps
-    this.mapDisplay = new google.maps.Map(selector, options)
-  },
-  getPosition: function(lat, lon) {
-    return new this.gMap.LatLng(parseFloat(lat),parseFloat(lon));
-},
-  _createPin: function(position, name) {
-    var newPin = new this.gMap.Marker({
-      position: position,
-      map: this.mapDisplay,
-      draggable: false
-    });
-    newPin.name = name;
-    this.allPins.push(newPin);
-    return newPin;
-  },
-  addPin: function(latlongobject, name) {
-    var pin = this._createPin(latlongobject, name);
-  },
-  getAjaxData: function(url, type, data) {
-    var xhr = $.ajax({
-      url: url,
-      type: type,
-      data: data,
-      dataType: "json"
-    })
-    return xhr;
-  },
-  loadTripPins: function(tripid){
-    var url = '/trips/' + tripid + '/markers';
-    var type = "get";
-    (this.getAjaxData(url, type)).done( function(resp) {
-      for (var i = 0; i < resp.lpins.length; i++) {
-        Map.addPin( Map.getPosition(resp.lpins[i].coords[0], resp.lpins[i].coords[1]), resp.lpins[i].name );
-      }
-    });
-  }
+    init: function(selector, options) {
+        this.allPins = [];
+        this.gMap = google.maps;
+        this.mapDisplay = new google.maps.Map(selector, options);
+    },
+    _getPosition: function(lat, lon) {
+        return new this.gMap.LatLng(parseFloat(lat), parseFloat(lon));
+    },
+    _addPin: function(position, name) {
+        var _this = this;
+        var newPin = new this.gMap.Marker({
+            position: position,
+            map: this.mapDisplay,
+            draggable: false
+        });
+        newPin.name = name;
+        this.allPins.push(newPin);
+        return _this.newPin;
+    },
+    loadTripPins: function(tripid) {
+        $.ajax({
+            url: '/trips/' + tripid + '/locations',
+            dataType: "JSON"
+        })
+            .done(function(pins) {
+                for (var i = 0; i < pins.length; i++) {
+                    var position = Map._getPosition(pins[i].coords[0], pins[i].coords[1]);
+                    Map._addPin(position, pins[i].name);
+                }
+            });
+    }
 };
