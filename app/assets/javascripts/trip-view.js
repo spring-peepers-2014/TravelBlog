06f1427546b36c20b6a80b98a7b3221b1b132cfc
nealfennimore <fennimoren@gmail.com>
2014-06-19 21:00:46 -0400

    Added button creation and re-organized classes for elements

--- app/assets/javascripts/trip-view.js
+++ app/assets/javascripts/trip-view.js
@@ -22,21 +22,28 @@ Trip.Presenter.prototype = {
   },
   presentLocation: function() {
   var h3_tag = document.createElement(this.rootNodeType),
-      a_tag = document.createElement(this.secondNodeType);
-      list_group = document.createElement(this.thirdNodeType);
-      list_group_item = document.createElement(this.thirdNodeType);
+      a_tag = document.createElement(this.secondNodeType),
+      panel_default = document.createElement(this.thirdNodeType),
+      panel_heading = document.createElement(this.thirdNodeType),
+      add_post_label = document.createElement(this.secondNodeType),
+      new_button = document.createElement("button");
+      
+    panel_default.className = "panel panel-default panel-relative";
+    panel_heading.className = "panel-heading";
+    new_button.className = "label label-info add-post-label";
+    h3_tag.className = "h3-location";
 
-    list_group.className = "list-group";
-    list_group_item.className = "list-group-item";
-    h3_tag.className = "location-names list-group-item-heading";
+    new_button.textContent = "+";
 
     a_tag.setAttribute('href', '/locations/' + this.trip_id);
     a_tag.textContent = this.trip_name;
 
-    list_group.appendChild(list_group_item);
-    list_group_item.appendChild(h3_tag);
+    panel_default.appendChild(panel_heading);
+    panel_heading.appendChild(h3_tag);
     h3_tag.appendChild(a_tag);
+    panel_default.appendChild(add_post_label);
+    add_post_label.appendChild(new_button);
 
-    return list_group;
+    return panel_default;
   }
 };
